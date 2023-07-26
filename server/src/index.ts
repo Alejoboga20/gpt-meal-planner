cfe9b65bb11e01a0cb21b6494595476ced90bb53
Alejo Boga <alejoboga20@gmail.com>
2023-07-26 14:14:08 -0400

    setup server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,12 @@
-console.log('hello express test');
+import express from 'express';
+
+const defaultPort = 3000;
+
+/* Initilize Express Application */
+const app = express();
+const port = process.env.PORT || defaultPort;
+
+app.get('/', (_, res) => res.send('Hello World!'));
+
+/* Start Express Server */
+app.listen(port, () => console.log(`App listening on port ${port}!`));
