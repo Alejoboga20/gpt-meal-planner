ba57d2b86c4d78bb842c2c652bae381225413bfa
Alejo Boga <60139228+Alejoboga20@users.noreply.github.com>
2023-07-26 15:43:33 -0400

    Feat/setup server (#1)
    * setup server
    
    * setup router and controller
    
    * command to build project
    
    * add github files

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,14 @@
-console.log('hello express test');
+import express from 'express';
+import { RoutesVersion, router } from './routes/routes';
+
+const defaultPort = 3000;
+
+/* Initilize Express Application */
+const app = express();
+const port = process.env.PORT || defaultPort;
+
+/* Define Routes */
+app.use(RoutesVersion.v1, router);
+
+/* Start Express Server */
+app.listen(port, () => console.log(`App listening on port ${port}!`));
